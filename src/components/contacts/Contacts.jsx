d922cc93b0901578652b171ea9dfee8c9383f16b
Mohammed_dev <Djmohams123@gmail.com>
2021-07-10 05:24:50 +0100

    Create Components

---
+++ src/components/contacts/Contacts.jsx
@@ -0,0 +1,9 @@
+import "./Contacts.scss"
+
+export default function Contacts() {
+    return (
+        <div className= "Contact Me">
+            
+        </div>
+    )
+}
---
+++ src/components/works/works.jsx
@@ -0,0 +1,10 @@
+import "./Works.scss"
+
+
+export default function Works() {
+    return (
+        <div className = "Works">
+            
+        </div>
+    )
+}
