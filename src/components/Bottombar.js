e56151b857f858b35e0125f95f93e7ae292557f0
Mohammed_dev <Djmohams123@gmail.com>
2021-07-27 05:47:55 +0100

    Add footer

---
+++ src/components/Bottombar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import lookmark from "../Images/logo-Copy.svg";
+import facebook from "../Images/icon-facebook.svg";
+import twitter from "../Images/icon-twitter.svg";
+
+export default function Bottombar() {
+    return (
+        <div>
+            <footer className="bg-black py-8">
+                <div className="container flex flex-col md:flex-row items-center">
+                    <div className="flex flex-1 flex-wrap items-center justify-center md:justify-start gap-12">
+                        <img src={lookmark} alt="lookmark" className="text-white h-full rounded mb-2- shadow"/>
+                        <ul className="flex text-white uppercase gap-12 text-xs">
+                        <a href="/" className="font-medium text-white hover:text-red-600  py-2 xl:flex place-items-start md:flex-4 cursor-pointer">Features</a>
+                            <a href="/" className="font-medium text-white hover:text-red-600  py-2 xl:flex items-center md:flex-2 cursor-pointer">Pricing</a>
+                            <a href="/" className="font-medium text-white hover:text-red-600  py-2 xl:flex items-center md:flex-4 cursor-pointer">Contact</a>
+                        </ul>
+                    </div>
+                    <div className="flex gap-10 mt-12 md:mt-0 hover:text-red-600 cursor-pointer">
+                        <img src={facebook} alt="Facebook" className=""/>
+                        <img src={twitter} alt="Twitter" className=""/>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <div>
+            <div className="bg-indigo-700 text-white py-20">
+                <div className="container">
+                    <div className="sm:w-3/4 lg:w-2/4 mx-auto">
+                        <p className="font-light uppercase text-center mb-8">35,000+ ALREADY JOINED</p>
+                        <h1 className="text-3xl text-center">Stay up-to-date with what we're doing</h1>
+                        <div className="flex flex-col sm:flex-row gap-6 mt-8">
+                            <input type="text" placeholder="Enter your email address" className="focus:outline-none flex-1 px-2 py-3 rounded-md text-black"/>
+                            <button type="button" className="flex items-center justify-center border border-transparent btn bg-red-600 hover:bg-white hover:text-black">
+                                        Contact Us
+                                    </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
